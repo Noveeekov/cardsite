c3a622c496b61320bd941827bba8f1c282fb9a48
Sergei Novikov <sergei@noveeek>
2019-08-11 11:16:25 +0300

    First commit: start project, fist ftest, localhost

---
+++ functional_tests.py
@@ -0,0 +1,21 @@
+from selenium import webdriver
+import unittest
+
+class NewTests ( unittest.TestCase ):
+    '''мои тесты'''
+    
+    def setUp ( self ):
+        '''запуск'''
+        self.browser = webdriver.Firefox()
+
+    def tearDown ( self ):
+        '''завершение'''
+        self.browser.quit()
+
+    def test_start_server ( self ):
+        '''запуск локального сервера'''
+        self.browser.get( 'http://localhost:8000' )
+
+if __name__ == '__main__':
+    unittest.main()
+
