b18322e5b93c3305958e870db18c74f35cf7bf1a
sern <sergei@noveeek>
2020-01-29 09:42:54 +0300

    Twelfth commit: version 0.0.1

--- dev/dist/static/js/navigation.js
+++ dev/dist/static/js/navigation.js
@@ -1,31 +1,55 @@
 let title = document.querySelector( '.title' ),
-    openBtn = document.querySelector( '.open-btn' ),
+    openButton = document.querySelector( '.open-btn' ),
     navBar = document.querySelector( '.menu-nav' ),
-    titleTxt = document.querySelector( '.title-text' ),
+    titleText = document.querySelector( '.title-text' ),
     projects = document.querySelector('.templates'),
     blurSection = document.querySelector('.about'),
-    menuBtn = document.getElementById('switchBtn'),
-    menuCbx = document.getElementById('switch'),
+    projectButton = document.getElementById('switchBtn'),
+    projectCheckbox = document.getElementById('switch'),
     direction = 0;
 
 let translateCanvas = function () { // change position of title
-    'use strict';
+    "use strict";
 
     if( direction === 0 && window.pageYOffset > 0 ) { // up
         direction = -90;
-        navBar.style.visibility = 'visible';
-        openBtn.style.visibility = 'hidden';
-    } else if( window.pageYOffset === 0 ) { // down
-        direction = 0;
-        navBar.style.visibility = '';
-        openBtn.style.visibility = '';
-    }
+        // navBar.style.visibility = 'visible';
+        openButton.style.visibility = 'hidden';
+    } else if( window.pageYOffset === 0 && direction != 0) { // down
 
+        direction = 0;
+        // navBar.style.visibility = '';
+        openButton.style.visibility = '';
+    }
     title.style.transform =
     title.style.WebkitTransform =
     title.style.MsTransform = 
                 `translateY(${direction}%)`;
 };
+
+// $(window).bind("mousewheel DOMMouseScroll MozMousePixelScroll", function (event) {
+//     delta = parseInt(event.originalEvent.wheelDelta || -event.originalEvent.detail);
+//     if(delta >= 0) {
+//         console.log("top");
+//         direction = 0;
+//         navBar.style.visibility = '';
+//         openButton.style.visibility = '';
+//         title.style.transform =
+//         title.style.WebkitTransform =
+//         title.style.MsTransform = 
+//                     `translateY(${direction}%)`;
+//     }
+//     else {
+//         console.log("bottom");
+//         direction = -90;
+//         navBar.style.visibility = 'visible';
+//         openButton.style.visibility = 'hidden';
+//         title.style.transform =
+//         title.style.WebkitTransform =
+//         title.style.MsTransform = 
+//                     `translateY(${direction}%)`;
+//     }
+// });
 
 let open = function () { // change the position of title on click
     'use strict';
@@ -34,7 +58,7 @@ let open = function () { // change the position of title on click
 };
 
 let resetY = function () {
-    'use strict';
+    "use strict";
 
     if(window.pageYOffset != 0) {
         window.scrollTo(0, 0);
@@ -45,17 +69,35 @@ let viewProjects = function () {
     'use strict';
 
     if(direction === -90) {
-        if(menuCbx.checked) {
+        if(projectCheckbox.checked) {
             projects.style.display = '';
             blurSection.classList.remove('blur-background');
+
+            // title.style.display = "";
+
+            title.style.transform =
+            title.style.WebkitTransform =
+            title.style.MsTransform = 
+                        `translateY(${direction}%)`;
+
+            document.body.style.overflow = "";
         } else {
             projects.style.display = 'flex';
             blurSection.classList.add('blur-background');
+
+            // title.style.display = "none";
+
+            title.style.transform =
+            title.style.WebkitTransform =
+            title.style.MsTransform = 
+                        `translateY(${direction - 300}%)`;
+
+            document.body.style.overflow = "hidden";
         }
     }
 };
 
 window.addEventListener( 'DOMContentLoaded', resetY );
 window.addEventListener( 'scroll', translateCanvas );
-openBtn.addEventListener( 'click', open );
-menuBtn.addEventListener('click', viewProjects);
+openButton.addEventListener( 'click', open );
+projectButton.addEventListener('click', viewProjects);
